3d6f91e6c30b272e9d3518322e7bf2fc7d3b4509
TinyBird0 <berno.binkert@gmail.com>
2019-01-04 14:08:53 +0100

    added simple autocomplete

--- filefile.py
+++ filefile.py
@@ -14,7 +14,7 @@ def downloader():
         for element in data['data']:
             cards[element['name']] = element
         data = requests.get(data['next_page']).json()
-        print('I am on page ' + data['next_page'][-1])
+        print('I am on page ' + data['next_page'][-3:])
 
     # Grabs the last page
     for element in data['data']:
@@ -29,9 +29,18 @@ def downloader():
 def cardfinder():
     while True:
         cardname = input('Card pls:')
-        if cardname in cardict:
+        if cardname in cardict.keys():
             print(cardict[cardname]['image_uris']['png'])
             break
+        print('Did you mean any of these:')
+        print(autocomplete(cardname))
+
+def autocomplete(cardname):
+    candidates = []
+    for element in cardict.keys():
+        if element[:len(cardname)] == cardname:
+            candidates = candidates + element
+
 
 
 try:
